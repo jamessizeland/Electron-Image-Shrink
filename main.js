f605e11ae04782c25259a41713427389cc1df11d
James Sizeland <jamessizeland@gmail.com>
2020-12-02 21:11:32 +0000

    adds file icon and fixes html filepath

--- main.js
+++ main.js
@@ -7,9 +7,10 @@ function createMainWindow() {
     title: "ImageShrink",
     width: 500,
     height: 600,
+    icon: "./assets/Icon_256x256.png",
   });
   //   mainWindow.loadURL("https://devdocs.io");
-  mainWindow.loadFile(".app/index.html");
+  mainWindow.loadFile("./app/index.html");
 }
 
 app.on("ready", createMainWindow);
