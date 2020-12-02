248a0c3c8c6f6f339ea4e0ebc9a9b17ee4d907cb
James Sizeland <jamessizeland@gmail.com>
2020-12-02 21:32:07 +0000

    add dev environment settings and some mac specific options.

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
+// initialize required packages
 const { app, BrowserWindow } = require("electron");
+
+// set environment
+process.env.NODE_ENV = "development"; //shows our environment, we can set this explicitly
+const isDev = process.env.NODE_ENV !== "production";
+console.log(process.platform);
+const isMac = process.platform === "darwin";
 
 let mainWindow;
 
@@ -8,9 +15,22 @@ function createMainWindow() {
     width: 500,
     height: 600,
     icon: `${__dirname}/assets/Icon_256x256.png`,
+    resizable: isDev,
   });
   //   mainWindow.loadURL("https://devdocs.io");
   mainWindow.loadFile("./app/index.html");
 }
 
 app.on("ready", createMainWindow);
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
